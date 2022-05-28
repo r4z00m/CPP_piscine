91789e7af16936a14c399f2a6aeee0bd535590f2
teusebio <teusebio@student.21-school.ru>
2022-05-28 10:13:40 +0300

    module 04 add test

--- CPP Module 04/ex00/main.cpp
+++ CPP Module 04/ex00/main.cpp
@@ -21,13 +21,16 @@ int main(void) {
 
 	const WrongAnimal* meta1 = new WrongAnimal();
 	const WrongAnimal* i1 = new WrongCat();
+	const WrongCat* wrongCat = new WrongCat();
 
 	std::cout << i1->getType() << " " << std::endl;
 	i1->makeSound(); //will output the wrong sound!
 	meta1->makeSound();
+	wrongCat->makeSound();
 
 	delete meta1;
 	delete i1;
+	delete wrongCat;
 
 	return 0;
 }
